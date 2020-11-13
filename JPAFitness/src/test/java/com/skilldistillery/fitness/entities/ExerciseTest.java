19586ab52a6a72c84bad3c37a4c1c428ad18f194
Chris Hiles <chrishiles790@gmail.com>
2020-11-13 16:10:59 -0700

    test done

---
+++ JPAFitness/src/test/java/com/skilldistillery/fitness/entities/ExerciseTest.java
@@ -0,0 +1,49 @@
+package com.skilldistillery.fitness.entities;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ExerciseTest {
+
+	private static EntityManagerFactory emf;
+	private static EntityManager em;
+	private static Exercise ex;
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("FitnessPu");
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		emf.close();
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		ex = em.find(Exercise.class, 1);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		em.close();
+		ex = null;
+	}
+
+	@Test
+	void test() {
+		assertNotNull(ex);
+		assertEquals("Cardio", ex.getName());
+	}
+
+}
