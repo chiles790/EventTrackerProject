47dd2b1194790ef12cda4b7ecf13713fb5eabaff
Chris Hiles <chrishiles790@gmail.com>
2020-11-30 07:37:49 -0700

    angular changes

--- FitnessTracker/src/main/java/com/skilldistillery/fitness/controllers/ExerciseController.java
+++ FitnessTracker/src/main/java/com/skilldistillery/fitness/controllers/ExerciseController.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.skilldistillery.fitness.entities.Exercise;
 import com.skilldistillery.fitness.services.ExerciseService;
 
+@CrossOrigin({"*", "http://localhost:4205"})
 @RequestMapping("api")
 @RestController
 public class ExerciseController {
